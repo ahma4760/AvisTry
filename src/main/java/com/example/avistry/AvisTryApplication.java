ad4463db5a032c60fa6511c07cdeb2ab27619691
ahma4760 <ahma4760@stud.kea.dk>
2023-02-21 11:23:16 +0100

    Initial commit

---
+++ src/main/java/com/example/avistry/AvisTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.avistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvisTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/avistry/AvisTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
